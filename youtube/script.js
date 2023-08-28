eebd6769f4c661502d353a2592a16097adea8e7c
Peter Rupa <megatricycle@gmail.com>
2023-08-29 02:14:43 +0800

    Fix sync not working when not starting from the synced video (ex. starting from homepage)

--- youtube/script.js
+++ youtube/script.js
@@ -1,70 +1,71 @@
-// @TODO: handle url changes
+async function handleMessage(request, sender, sendResponse) {
+    if (request.message === 'fetch_youtube_metadata') {
+        const metadata = await getMetadata();
 
-async function run() {
-    const videoElement = await waitForElement('.video-stream');
-
-    const startDateTimeElement = await waitForElement(
-        'meta[itemprop="startDate"]'
-    );
-    const endDateTimeElement = await waitForElement('meta[itemprop="endDate"]');
-
-    const endDateTime = new Date(endDateTimeElement.getAttribute('content'));
-    const startDateTime = new Date(
-        startDateTimeElement.getAttribute('content')
-    );
-
-    // @TODO: can't differentiate between premieres and livestream...
-
-    // premiers have a varying countdown but it is part of startDate element
-    // hence, get the startDateTime by computing it rather than relying on startDate element
-    const _startDateTime = new Date(
-        endDateTime.getTime() - videoElement.duration * 1000
-    );
-
-    function handleTimeUpdate(event) {
-        chrome.runtime.sendMessage({
-            message: 'youtube_timeupdate',
-            timestamp: new Date(
-                startDateTime.getTime() + event.target.currentTime * 1000
-            ).toISOString(),
+        sendResponse({
+            id: new URL(window.location).searchParams.get('v'),
+            title: metadata.name,
+            channelTitle: metadata.author,
+            thumbnail: metadata.thumbnailUrl[0],
         });
+
+        return;
     }
 
-    chrome.runtime.sendMessage({
-        message: 'youtube_active',
-    });
+    if (request.message === 'youtube_start') {
+        console.log('START');
 
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        if (request.message === 'fetch_youtube_metadata') {
-            const scriptTag = document.querySelector('#scriptTag');
+        const metadata = await getMetadata();
 
-            const metadata = JSON.parse(scriptTag.textContent);
+        const videoElement = await waitForElement('.video-stream');
 
-            sendResponse({
-                id: new URL(window.location).searchParams.get('v'),
-                title: metadata.name,
-                channelTitle: metadata.author,
-                thumbnail: metadata.thumbnailUrl[0],
+        const endDateTime = new Date(metadata.publication[0].endDate);
+        const startDateTime = new Date(metadata.publication[0].startDate);
+
+        // @TODO: can't differentiate between premieres and livestream...
+
+        // premiers have a varying countdown but it is part of startDate element
+        // hence, get the startDateTime by computing it rather than relying on startDate element
+        const _startDateTime = new Date(
+            endDateTime.getTime() - videoElement.duration * 1000
+        );
+
+        function handleTimeUpdate(event) {
+            chrome.runtime.sendMessage({
+                message: 'youtube_timeupdate',
+                timestamp: new Date(
+                    startDateTime.getTime() + event.target.currentTime * 1000
+                ).toISOString(),
             });
-
-            return;
         }
 
-        if (request.message === 'youtube_start') {
-            console.log('YouTube Discord VOD initialized.');
+        console.log('YouTube Discord VOD initialized.');
 
-            videoElement.addEventListener('timeupdate', handleTimeUpdate);
-        }
+        videoElement.addEventListener('timeupdate', handleTimeUpdate);
+    }
 
-        if (request.message === 'youtube_cancel') {
-            console.log('YouTube Discord VOD stopped.');
+    if (request.message === 'youtube_cancel') {
+        console.log('YouTube Discord VOD stopped.');
 
-            videoElement.removeEventListener('timeupdate', handleTimeUpdate);
-        }
-    });
+        videoElement.removeEventListener('timeupdate', handleTimeUpdate);
+    }
 }
 
-run();
+chrome.runtime.sendMessage({
+    message: 'youtube_active',
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    handleMessage(request, sender, sendResponse);
+});
+
+async function getMetadata() {
+    const scriptTag = await waitForElement('#scriptTag');
+
+    const metadata = JSON.parse(scriptTag.textContent);
+
+    return metadata;
+}
 
 // https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
 function waitForElement(selector) {
