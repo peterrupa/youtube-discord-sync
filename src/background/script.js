a0e4139ef8762103e75d7aaea1204f925290bd3a
megatricycle <megatricycle@gmail.com>
2026-09-21 20:20:07 +0800

    Revert 9a15f5fa854e67209983981281ee6705913ee869

--- src/background/script.js
+++ src/background/script.js
@@ -16,40 +16,6 @@ async function handleMessage(request, sender, sendResponse) {
             message: 'sync_stop',
             tabId: sender.tab.id,
         });
-
-        const youtubeTabsRaw = await chrome.tabs.query({
-            url: [
-                'https://www.youtube.com/watch*',
-                'https://www.youtube.com/live/*',
-            ],
-        });
-
-        let activeSyncsCount = 0;
-
-        for (const tab of youtubeTabsRaw) {
-            if (!tab.id) {
-                continue;
-            }
-
-            const response = await chrome.tabs.sendMessage(tab.id, {
-                message: 'fetch_info',
-            });
-
-            if (response?.activeSyncs?.length > 0) {
-                activeSyncsCount = activeSyncsCount + 1;
-            }
-        }
-
-        if (activeSyncsCount === 0) {
-            browser.action.setIcon({
-                path: {
-                    16: '../assets/16_gray.png',
-                    32: '../assets/32_gray.png',
-                    48: '../assets/48_gray.png',
-                    128: '../assets/128_gray.png',
-                },
-            });
-        }
     }
 }
 
@@ -60,16 +26,6 @@ async function run() {
         handleMessage(request, sender, sendResponse);
 
         return true;
-    });
-
-    // initially, set all icons to gray
-    browser.action.setIcon({
-        path: {
-            16: '../assets/16_gray.png',
-            32: '../assets/32_gray.png',
-            48: '../assets/48_gray.png',
-            128: '../assets/128_gray.png',
-        },
     });
 }
 
